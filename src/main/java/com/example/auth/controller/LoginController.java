81e2a6be6e49030dfabc916f94f915c8c8124d5c
wll27471297 <wll27471297@gmail.com>
2022-07-03 00:57:45 +0900

    MockMvc Post Test

--- src/main/java/com/example/auth/controller/LoginController.java
+++ src/main/java/com/example/auth/controller/LoginController.java
@@ -1,18 +1,29 @@
 package com.example.auth.controller;
 
+import com.example.auth.domain.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 
 @RestController
 @RequestMapping("login")
 public class LoginController {
 
+    Logger logger = LoggerFactory.getLogger(LoginController.class);
+
     @PostMapping
-    public ResponseEntity login() {
-        return new ResponseEntity(HttpStatus.OK);
+    public ResponseEntity<User.Response> login(@RequestBody User.Request user) {
+        logger.info(user.toString());
+
+        User.Response response = new User.Response();
+        response.setInfo(new User.Info(1L, user.getEmail()));
+        return new ResponseEntity(response, HttpStatus.OK);
     }
 }
--- src/main/java/com/example/auth/domain/User.java
+++ src/main/java/com/example/auth/domain/User.java
@@ -1,10 +1,32 @@
 package com.example.auth.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
-@ToString
 public class User {
-    Long id;
-    String email;
-    String passwd;
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class Info {
+        private Long id;
+        private String email;
+    }
+
+    @Getter
+    @ToString
+    @AllArgsConstructor
+    public static class Request {
+        private String email;
+        private String passwd;
+    }
+
+    @Getter
+    @Setter
+    public static class Response {
+        private Info info;
+    }
+
 }
